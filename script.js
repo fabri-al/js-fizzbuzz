8f3d7a9f2e88750334d501863ed704286d3892d6
MyUsername <my@email.com>
2025-10-30 11:24:05 +0100

    ordered comments

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const replaceFizzBuzz = "FizzBuzz"; //creo una variabile costante
 
 
 //Svoglimento
+
 // creo il ciclo for
 for (i = 0; i <= 100; i++) { //(dichiaro la variabile 'i' = 0; se 'i' è minore o uguale a 100; incrementa 'i')
 
     document.getElementById("ciclo").innerHTML += `<div>${i}</div>`; //inserisco la variabile 'i' all'interno del div che 
-    // ho creato nel file html e, ciclando, viene stampata
-    //in pagina ogni volta con un numero, da 0 a 100
-
+                                                                    // ho creato nel file html e, ciclando, viene stampata
+                                                                    //in pagina ogni volta con un numero, da 0 a 100
 
     //creo una condizione che verifica se, 'i' è divisibile sia per 3 che per 5
     if (i % 3 == 0 && i % 5 == 0) {
